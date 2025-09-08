bf957a23b13c7cb9131d0a7278342a09392cfeac
Jesper Morais <jeppe.morris@hotmail.se>
2025-09-08 21:37:08 +0200

    add: nvs and nimble init

--- firmware_embedded/main/main.c
+++ firmware_embedded/main/main.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
+#include "esp_hidh_nimble.h"
+#include "esp_check.h"
+#include "nvs_flash.h"
+
+char* TAG = "MAIN";
 
 void app_main(void)
 {
+    int rc;
+    esp_err_t ret;
+
+    ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES ||
+        ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+            ESP_ERROR_CHECK(nvs_flash_erase());
+            ret = nvs_flash_init();
+    }
+    if (ret != ESP_OK){
+        ESP_LOGE(TAG, "failed to initialize nvs flash, error code: %d ", ret);
+    }
+
+    // INIT Nimble stack
+    ret = nimble_port_init();
+    if (ret != ESP_OK){
+        ESP_LOGE(TAG, "Failed to init Nimble stack error code %d", ret);
+        return;
+    }
+
+    
 
 }
