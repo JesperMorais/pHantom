a7d280c43214a67f42c619f51112a800c577bcec
Jesper Morais <jeppe.morris@hotmail.se>
2025-09-08 21:45:42 +0200

    add: gap stuff

---
+++ firmware_embedded/main/gap.c
@@ -0,0 +1,32 @@
+#include "gap.h"
+#include "esp_gap_ble_api.h"
+#include "services/gap/ble_svc_gap.h"
+
+const char* DEVICE_NAME = "Jesp_Device";
+const char* TAG = "GAP";
+
+int gap_init(){
+    int rc = 0;
+
+    // INIT GAP service
+    ble_svc_gap_init();
+
+    //Set ble name?? TO fukicng waht?? VOID?
+    rc = ble_svc_gap_device_name_set(DEVICE_NAME);
+    if (rc != 0){
+        ESP_LOGE(TAG, "Failed to set device name %s, error code: %d ", DEVICE_NAME, rc);
+        return rc;
+    }
+
+    int randomID = 4;
+    /* Code example had BLE_GAP_APPEARANCE_GENERIC_TAG??
+        We test with random int for now? Could it be some tag for the device?
+    */
+    rc = ble_svc_gap_device_appearance_set(randomID);
+    if (rc != 0){
+        ESP_LOGE(TAG, "Failed to set device appearance, error code: %d ", rc);
+        return rc;
+    }
+
+    return rc;
+}
